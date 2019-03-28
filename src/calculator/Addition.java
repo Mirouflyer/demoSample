0518b3aafe2d908d8feb2e9f5f2e46eedaf6b332
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-28 14:42:03 -0400

    added div method

--- src/calculator/Addition.java
+++ src/calculator/Addition.java
@@ -3,8 +3,12 @@ package calculator;
 public class Addition {
 
     public static int add(int a, int b){
-
         int result = a + b;
+        return result;
+    }
+
+    public static int div(int a, int b){
+        int result = a/b;
         return result;
     }
 
