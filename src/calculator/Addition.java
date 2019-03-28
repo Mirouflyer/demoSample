0e27ae5256b2981edb546d25a27ce4f927a93586
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-28 14:39:00 -0400

    created a new method

---
+++ src/calculator/Addition.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Addition {
+
+    public static int add(int a, int b){
+
+        int result = a + b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int addition = add (23,24);
+
+    }
+}
