9537704bd200a7f1140058fd850e00c5b2f0819d
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-28 15:41:24 -0400

    added a new class Array

---
+++ src/loop/Array.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Array {
+
+    public void students(){
+        String [] list = new String[5];
+        list[0] ="Said";
+        list[1]="Kaci";
+        list[2]="Mhend";
+        list[3]="Achour";
+        list[4]="Oulhadj";
+        for(int i = 0;i<list.length;i++){
+            System.out.println(list[i]);
+
+        }
+    }
+}
